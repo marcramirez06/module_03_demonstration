8e575f8d5151147648d698806d88e338088b5ef5
marcramirez06 <mramirez3@rrc.ca>
2024-01-31 17:12:16 -0600

    Added a commit with all the files from the zip file provided to us by the instructor.

---
+++ module_03_logic.py
@@ -0,0 +1,58 @@
+"""
+Description: Introduction to Logic
+Author: Student Name
+Date: September 2023
+Usage: To execute, press the play button in the VSC IDE.
+"""
+
+# Variables used in this demonstration:
+age = 18
+sibling_age = 9
+horizontal_position = 3
+vertical_position = 5
+number = 5
+fruits = ["apple", "banana", "cherry"]
+
+
+# CONDITIONALS
+
+
+# NESTED CONDITIONALS
+
+# COMPARISON OPERATORS:
+
+# == equality.  Returns True if both operands are equal, false otherwise.
+# != inequality. Returns True if operands are not equal, false otherwise.
+# < less than. Returns True if first operand is less than second, false otherwise.
+# > greater than. Returns True if first operand is greater than second, false otherwise.
+# <= less than or equal.Returns True if first operand is less than or equal to second, false otherwise.
+# >= greater than or equal. Returns True if first operand is greater than or equal to second, false otherwise.
+
+# Examples:
+"""
+first_operand = 5
+second_operand = 10
+print(first_operand == second_operand) # False
+print(first_operand != second_operand) # True
+print(first_operand < second_operand) # True
+print(first_operand > second_operand) # False
+print(first_operand <= second_operand) # True
+print(first_operand >= second_operand) # False
+"""
+
+# LOGICAL OPERATORS
+
+
+
+# TERNARY EXPRESSION
+# given expression:
+"""
+if number % 2 == 0:
+    result = "Even"
+else:
+    result = "Odd"
+
+print("result")
+"""
+
+# MEMBERSHIP CHECKING
